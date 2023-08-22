ef19572edeba46392b889e9941e6d7c42d0f92e9
einstellung <kekedog@126.com>
2023-08-22 14:55:24 +0800

    part3: add cli

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ mod blockchain;
 mod bcdb;
 mod proofofwork;
 mod utils;
+mod cli;
 
 use blockchain::Blockchain;
+use cli::CLI;
 
 fn main() {
-    let mut blockchain = Blockchain::new();
-    blockchain.add_block("Send 1 BTC to Alice");
-    blockchain.add_block("Send 2 BTC to Bob");
-    blockchain.print_block();
+    let blockchain = Blockchain::new();
+    // blockchain.add_block("Send 1 BTC to Alice");
+    // blockchain.add_block("Send 2 BTC to Bob");
+    // blockchain.print_block();
 
+    let mut cli = CLI::new(blockchain);
+    cli.run();
 }
